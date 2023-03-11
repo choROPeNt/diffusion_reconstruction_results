06e47bff63bbb8460ffbe498974136010114e5d2
choROPeNt <christian.duereth@tu-dresden.de>
2023-03-11 14:29:53 +0100

    update for greyscale diffusion samples

--- vizualize_samples.py
+++ vizualize_samples.py
@@ -4,6 +4,7 @@ import numpy as np
 from PIL import Image
 from numpy import load
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import argparse
 
 
@@ -41,11 +42,13 @@ def main():
 
 
         for n, (img, cl) in enumerate(zip(imgs,cls)):
-            
+            if img.shape[2] == 1:
+                img = img.reshape(img.shape[0],img.shape[1])
+
             ax = fig.add_subplot(rows,cols,n+1)
             im = Image.fromarray(img)
 
-            ax.imshow(im)
+            ax.imshow(im,cmap=mpl.colormaps['binary'])
             ax.set_title(cl)
             ax.axis('off')
             if DIR is not None:
